febadf039e255db898de32c0f459855e650ef458
blessybaiju <jayson.kiptoo@gmail.com>
2024-04-28 14:26:08 +0300

    learning scraping with javascript using cheerio

---
+++ index.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function scrapeSite(keyword){
+    const url = `https://www.google.com/search?q=${keyword}&tbm=isch`;
+	const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+	return $
+}
+
+const keyword = "coffee"; // change with any keyword you want
+scrapeSite(keyword).then(result => {
+	console.log(result)
+	}).catch(err => console.log(err));
