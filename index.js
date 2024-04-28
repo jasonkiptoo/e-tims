c7ba84ff50d1a18021041f76286251fba6f32bc7
blessybaiju <jayson.kiptoo@gmail.com>
2024-04-28 14:36:04 +0300

    new addition to scrap etims

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function scrapeSite(keyword){
-    const url = `https://www.google.com/search?q=${keyword}&tbm=isch`;
+async function scrapeSite(keyword) {
+	const url = `https://etims.kra.go.ke/common/link/etims/receipt/indexEtimsReceiptData?Data=A003233471M00WDB6GNG3O3DKZ4NK`;
 	const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
-	return $
+	
+	const $ = cheerio.load(data);
+
+	
+	const spans = [];
+$('span').each((_idx, el) => {
+	const span = $(el).text();
+	spans.push(span);
+});
+
+return spans;
 }
 
 const keyword = "coffee"; // change with any keyword you want
