0ffac2a309a9e99ff6ffe856baf0bab95dcec218
blessybaiju <jayson.kiptoo@gmail.com>
2024-05-01 20:43:59 +0300

    new fix to get headers

--- services/getData.js
+++ services/getData.js
@@ -1,45 +1,64 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+// const axios = require('axios');
+// const cheerio = require('cheerio');
 
+// async function scrapeSite() {
 
+//     const url = `https://etims.kra.go.ke/common/link/etims/receipt/indexEtimsReceiptData?Data=A003233471M00WDB6GNG3O3DKZ4NK`;
+//     const { data } = await axios.get(url);
+//     const $ = cheerio.load(data);
 
-const scrapeMe= (url)=>{
+//     const clientDetails = [];
 
-    scrapeSite(url)
+//     $('div.topinfo').each((i, el) => {
+//         const text = $(el).text().replace(/\s+/g, ' ').trim(); // Replace multiple spaces with a single space
+//         const cleanedText = text.replace(/\n|\t/g, ''); // Remove '\n' and '\t' characters
+//         const lines = cleanedText.split('\n').map(line => line.trim()).filter(line => line !== '');
 
-}
+//         clientDetails.push(lines);
+//     });
 
-
-
-async function scrapeSite() {
-
-    const url = `https://etims.kra.go.ke/common/link/etims/receipt/indexEtimsReceiptData?Data=A003233471M00WDB6GNG3O3DKZ4NK`;
-    console.log(url, "url");
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
-
-    const clientDetails = [];
-
-    $('div.topinfo').each((i, el) => {
-        const text = $(el).text().replace(/\s+/g, ' ').trim(); // Replace multiple spaces with a single space
-        const cleanedText = text.replace(/\n|\t/g, ''); // Remove '\n' and '\t' characters
-        const lines = cleanedText.split('\n').map(line => line.trim()).filter(line => line !== '');
-
-        clientDetails.push(lines);
-    });
-
-    // const {kraPin, supplierName, invoiceDate, invoiceNumber, Item, taxableAmount, totalVat, totalInvoice} = 
+//     // const {kraPin, supplierName, invoiceDate, invoiceNumber, Item, taxableAmount, totalVat, totalInvoice} = 
 
     
 
 
+//     return clientDetails;
+// }
+
+// scrapeSite().then(result => {
+
+//     console.log(result);
+//     return result
+// }).catch(err => console.log(err));
+
+// // module.exports = {scrapeMe, scrapeSite}
+
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function scrapeSite() {
+    const url = `https://itax.kra.go.ke/KRA-Portal/invoiceChk.htm?actionCode=loadPage&invoiceNo=0040804130000058920`;
+
+    const { data } = await axios.get(url);
+
+    const $ = cheerio.load(data);
+
+    const clientDetails = {};
+
+    $('tr').each((i, el) => {
+        const key = $(el).find('td').eq(0).text().trim().replace(/\n|\t/g, '');
+        const value = $(el).find('td').eq(1).text().trim().replace(/\n|\t/g, '');
+        if (key && value) { // To avoid empty keys or values
+            clientDetails[key] = value;
+        }
+    });
+
     return clientDetails;
 }
 
-scrapeSite().then(result => {
-
-    console.log(result);
-    return result
-}).catch(err => console.log(err));
-
-module.exports = {scrapeMe, scrapeSite}
+scrapeSite()
+    .then(result => {
+        console.log(JSON.stringify(result, null, 2));
+    })
+    .catch(err => console.log(err));
