cc5f23bda12d6b8d5084097486da7dba8a1d333a
Jason Kiptoo <kiptoojason@gmail.com>
2024-05-24 15:34:44 +0300

    fix to router

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/",(req,res)=>{
+app.get("/",(req,res)=>{
   res.send("Application up and running")
 })
 app.use(router);
