06a805c34f1d63946a5c48ab397f3dcbad314f02
Jason Kiptoo <kiptoojason@gmail.com>
2024-05-24 15:24:58 +0300

    new fix

--- index.js
+++ index.js
@@ -62,5 +62,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use("/",(req,res)=>{
+  res.send("Application up and running")
+})
 app.use(router);
